9584a073ba795c2649d61bbd534a2f13f69a1441
Parag Sharma <paragsharma602@gmail.com>
2025-08-22 17:46:23 +0530

    app.py

--- app.py
+++ app.py
@@ -46,3 +46,11 @@ def full_search_api():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
